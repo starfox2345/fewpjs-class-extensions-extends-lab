99ccc19e9faf67744eb0260f7534bc514b260455
yatsun <yatsunlam25@gmail.com>
2021-09-24 00:52:22 -0400

    commit tree aff8a907da1bc1bed81a8d07d32d8a0d51d752b3

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+
+class Polygon {
+    constructor(sides) {
+        this.sides = sides
+        this.count = this.sides.length
+    }
+
+    get countSides(){
+        return this.sides.length
+    }
+
+    get perimeter(){
+        let sum = 0;
+        let i = 0;
+        while (i < this.sides.length){
+            sum += this.sides[i];
+            i++;
+        }
+        return sum
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        if (this.count === 3){
+            let sideOne = this.sides[0]
+            let sideTwo = this.sides[1]
+            let sideThree = this.sides[2]
+            return ((sideOne + sideTwo > sideThree) && (sideOne + sideThree > sideTwo) && (sideTwo + sideThree > sideOne))
+            // {return true} else {return false}
+        // } else {
+        //     return false        
+        }
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        if (this.count === 4) {
+            let sideOne = this.sides[0]
+            let sideTwo = this.sides[1]
+            let sideThree = this.sides[2]
+            let sideFour = this.sides[3]
+            return ((sideOne * sideTwo) === (sideThree * sideFour))
+        }
+    }
+
+    get area(){
+        return this.sides[0]**2
+    }
+}
